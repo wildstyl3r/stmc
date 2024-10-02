7839641548ea30c455d113a6b09688479ce3e956
Mike <wildstyl3r@yandex.ru>
2024-10-02 12:37:44 +0300

    move config loading to parameters.go

--- main.go
+++ main.go
@@ -11,27 +11,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/BurntSushi/toml"
 	"github.com/wildstyl3r/lxgata"
 )
-
-type Config struct {
-	OutputDir string
-	Models    map[string]ModelParameters
-
-	// to reset global defaults
-	CrossSections        string
-	GapLength            float64 // [m]
-	CathodeFallLength    float64 // [m]
-	CathodeFallPotential float64 // [V]
-	CathodeCurrent       float64 // [A m^-2] = [C s^-1 m^-2]
-	ConstEField          float64 // [V / m]
-	Temperature          float64 // [K]
-	DeltaE               float64 // [eV]
-	DeltaMu              float64
-	NElectrons           int
-	MakeDir              bool
-}
 
 //AINF
 // AIV?
@@ -55,19 +36,7 @@ func main() {
 	startTime := time.Now()
 	fmt.Printf("Current time: %s\n", startTime.UTC().Format(time.UnixDate))
 
-	configFileName := strings.TrimSuffix(*configFileNamePointer, ".toml")
-
-	var config Config
-	meta, err := toml.DecodeFile(configFileName+".toml", &config)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	if len(config.Models) == 0 {
-		fmt.Println("No models provided")
-		os.Exit(0)
-	}
+	var config, meta = loadConfig(strings.TrimSuffix(*configFileNamePointer, ".toml"))
 
 	if config.OutputDir != "" && config.OutputDir != "." {
 		os.MkdirAll(config.OutputDir, 0750)
--- parameters.go
+++ parameters.go
@@ -2,9 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/BurntSushi/toml"
 )
+
+type Config struct {
+	OutputDir string
+	Models    map[string]ModelParameters
+
+	// to reset global defaults
+	CrossSections        string
+	GapLength            float64 // [m]
+	CathodeFallLength    float64 // [m]
+	CathodeFallPotential float64 // [V]
+	CathodeCurrent       float64 // [A m^-2] = [C s^-1 m^-2]
+	ConstEField          float64 // [V / m]
+	Temperature          float64 // [K]
+	DeltaE               float64 // [eV]
+	DeltaMu              float64
+	NElectrons           int
+	MakeDir              bool
+}
+
+func loadConfig(configFileName string) (Config, toml.MetaData) {
+	var config Config
+	meta, err := toml.DecodeFile(configFileName+".toml", &config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if len(config.Models) == 0 {
+		fmt.Println("No models provided")
+		os.Exit(0)
+	}
+	return config, meta
+}
 
 type ModelParameters struct {
 	CrossSections        string
